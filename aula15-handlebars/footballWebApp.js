23574c641ac6255156fd94ef400f9decb47c7037
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-10-16 12:05:41 +0100

    Add views and Handlbars tenplate engine

--- aula15-handlebars/footballWebApp.js
+++ aula15-handlebars/footballWebApp.js
@@ -1,5 +1,7 @@
 const http = require('http')
 const url = require('url')
+const fs = require('fs')
+const hbs = require('handlebars')
 const foot = require('./footballService')
 const port = 3000
 
@@ -20,10 +22,11 @@ const routes = {
 
 function router(req, resp) {
     const urlObj = url.parse(req.url, true)
-    const action = routes[urlObj.pathname.substring(1)]
+    const actionName = urlObj.pathname.substring(1)
+    const action = routes[actionName]
     if(action != undefined) {
         const parameters = mapParameters(urlObj.query, action)
-        parameters.push(actionCallback(resp))
+        parameters.push(actionCallback(resp, actionName))
         action.apply(this, parameters)
     } else {
         resp.statusCode = 404 // Resource Not Found
@@ -35,19 +38,32 @@ function router(req, resp) {
  * 2. Representação: Obter uma String com a representação HTML do recurso.
  * 3. Envio da resposta: statusCode 200 + setHeader() + end()
  */
-function actionCallback(resp) {
+function actionCallback(resp, actionName) {
     return (err, obj) => {
         let data
         if(err) {
             data = err.message
             resp.statusCode = 500
         } else {
-            data =  JSON.stringify(obj)
+            data =  view(obj, './views/' + actionName + 'View.hbs')
             resp.statusCode = 200
         }
         resp.setHeader('Content-Type', 'text/html')
         resp.end(data)
     }
+}
+
+
+/**
+ * Returns an Html view.
+ * 
+ * @param {*} ctx Handlebars context object
+ * @param {*} viewPath Path for handlebars template source.
+ */
+function view(ctx, viewPath) {
+    const viewSrc = fs.readFileSync(viewPath).toString()
+    const v = hbs.compile(viewSrc)
+    return v(ctx)
 }
 
 function mapParameters(query,func){
