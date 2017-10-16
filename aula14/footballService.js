452e5d93656130c9e32a7e4c0ca379e35c1aca17
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-10-16 11:32:25 +0100

    add getLeagueTable

--- aula14/footballService.js
+++ aula14/footballService.js
@@ -18,6 +18,12 @@ function getLeagues(cb) {
 }
 
 function getLeagueTable(leagueId, cb) {
+    const path = `http://api.football-data.org/v1/soccerseasons/${leagueId}/leagueTable`
+    req(path, (err, res, data) => {
+        if(err) return cb(err)
+        const obj = JSON.parse(data.toString())
+        cb(null, obj.standing)
+    })
 }
 
 function getTeam(teamId, cb) {
