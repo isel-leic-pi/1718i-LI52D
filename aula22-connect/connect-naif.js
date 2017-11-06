b0d4077105a6cbd519781ce096a155ff5d022461
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-11-06 10:16:36 +0000

    TPC06 - solution 2

--- aula22-connect/connect-naif.js
+++ aula22-connect/connect-naif.js
@@ -5,29 +5,25 @@ module.exports = initPipeline
 const arr = [] // stores the Middlewares
 
 function initPipeline() {
-
-    let idx = 0
     router.use = addMw
     return router
-
 
     /**
      * Handler for HTTP requests.
      */
     function router(req, res) {
-        var mw = arr[idx]
-        mw(req, res, () => next(req, res, idx) )
+        let idx = 0
+        next()
+        function next() {
+            const mw = arr[idx++] // Get the next middleware
+            mw(req, res, next)    // Call the middleware 
+        }
     }
     
     /**
      * Adds a mw to the arr array.
      */
     function addMw(mw) {
-        return arr.push(mw)
-    }
-
-    function next(req, res, idx) {
-        var mw = arr[++idx]
-        return mw( req, res, () => next(req,res, idx))
+        arr.push(mw)
     }
 }
