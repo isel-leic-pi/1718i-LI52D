0086eb9d8b9f625c0c2b06f79552c8ab34d79a32
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-11-16 12:18:18 +0000

    Setup aula 28

---
+++ aula28/routes/userRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+const userService = require('./userService')
+const passport = require('passport')
+
+module.exports = router
+
+router.get('/login', (req, res) => {
+    res.render('login', {layout: false})
+})
+
+router.post('/login', (req, res, next) => {
+    userService.authenticate(req.body.username, req.body.password, (err, user, info) => {
+        if(err) return next(err)
+        if(info) return next(new Error(info))
+        req.logIn(user, (err) => {
+            if(err) return next(err)
+            res.redirect('/leagues')
+        })
+    })
+})
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user.username)
+  })
+  
+passport.deserializeUser(function(username, cb) {
+    userService.find(username, cb)
+})
+
+  
